667915808f2bcb121dc04dac2b2e629f4791369f
Everlyne22 <everlynenjuguna24@gmail.com>
2022-05-17 17:06:39 +0300

    lesson 3

--- lesson3.py
+++ lesson3.py
@@ -1,6 +1,32 @@
-#getting user input
-my_first_name =input("Everlyne")
-print("What is my name?")
-print("my name is " + my_first_name)
+#getting user input(you can answer without the input you can't)
+f_question = input("What is your name? ")
+
+print("I am " + f_question)
+
 age =input("How old are you?")
-print("You are"+ str (age) + "years old.")
+
+print("You are "+ str (age) + "years old.")
+
+placeofbirth =input("Where were you born? ")
+
+print("I was born in " + placeofbirth)
+
+dateofbirth = input("When is your birthday? ")
+
+print("My birthday is on the " + dateofbirth)
+
+numberofsiblings = input("How many siblings do youn have? ")
+
+print("I have " + str (numberofsiblings))
+
+favoriteshow =input("What is your favourite show? ")
+
+print("I love to watch " + favoriteshow)
+
+chairman = input("Who is the chairman of KEBS?")
+
+print(" The chairman of KEBS is " + chairman)
+
+tattoos = input("How many tattoos do you have? ")
+
+print("I have " + tattoos)
