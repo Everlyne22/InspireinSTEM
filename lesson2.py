5cc56b63681888bc6d68601178e0df01bd7cee29
Everlyne22 <everlynenjuguna24@gmail.com>
2022-05-17 12:04:37 +0300

    lesson 2

--- lesson2.py
+++ lesson2.py
@@ -1,6 +1,11 @@
+#getting user input
 my_first_name="Everlyne"
 print("I am " + my_first_name)
 school_attended="Loreto Limuru"
 print("I attended " + school_attended)
 age= "17"
 print("I am " + str (age) + " years old")
+dateofbirth = "24th September 2004"
+print("My birthday is on " + dateofbirth)
+favouritemovie= "The Witcher"
+print("I love to watch " + favouritemovie)
