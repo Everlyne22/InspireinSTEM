9c4508633811007f017d100ada3ddaec7e4e5cab
Everlyne22 <everlynenjuguna24@gmail.com>
2022-05-18 11:16:32 +0300

    README.md

---
+++ lesson5.py
@@ -0,0 +1,57 @@
+#stringmethods, f, dot format, string format, R strip
+
+
+#name =  "    Neema Gathoni   "
+
+
+#username = rstrip() (name)
+
+
+#age = "18"
+
+
+#age = "55"
+
+
+#person = "I am " + str (name) + "and I am " + str(age)
+#print(person)
+
+
+#print(f"I am " + str (name) + "and my age is " + str(age))
+
+
+#adding new line and tab
+#print (f"name")
+
+
+
+#str(), int(), float()
+
+
+x = 40
+
+
+print = (int(x))
+
+print = (float (x))
+
+f_name = "Hate"
+
+s_name = "Adah"
+
+full_name= "f_name" + "s_name"
+
+print = ("full_name")
+
+name = "Theodore"
+
+print ("name.replace ('t' , 'g')")
+print (""split(Theodore")
+def jls_extract_def():
+    return str
+
+
+print (str (length))
+
+
+
