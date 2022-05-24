4fdd25a92ccd37b502a2cf3cae6a7d1185ae0153
Everlyne22 <everlynenjuguna24@gmail.com>
2022-05-24 22:46:31 +0300

    lesson 8

--- lesson8.py
+++ lesson8.py
@@ -3,4 +3,27 @@
     #Name: Everlyne Njuguna
     #Date: 24/05/2022
 ##############################
-#Dictionaries
+
+#Initialising Dictionaries
+Student = {"Name": "Memphis" , "Age" : "21" , "Gender": "Female"}
+
+#Accesing values in a dictionary
+print(Student["Name"])
+print(Student["Age"])
+print(Student["Gender"])
+
+#Adding values to a dictionary
+Student["ID No."] = "28900563"
+Student["Hobby"] = "Swimming"
+Student["Favourite Food"] = "Chicken Nuggets"
+print(Student)
+
+#Empty Dictionary
+Student = {}
+Student["Hometown"] = "Melbourne"
+Student["Club"] = "Law Poll"
+print(Student)
+
+#deleting values from a dictionary
+del Student["Club"]
+print(Student)
